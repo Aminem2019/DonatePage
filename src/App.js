0aec3e319b4ff13d9d4ad656348aced118fef134
aminem2019 <aminemw29@gmail.com>
2023-02-22 10:56:05 +0000

    Add currency conversion to show/enter it in USD (BTC next?)

--- src/App.js
+++ src/App.js
@@ -1,103 +1,162 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "./App.css";
+import { convertToUsd, convertToCurrency, chainIdToCurrencyCode} from "./utils.js";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
   const [walletChainId, setWalletChainId] = useState("");
   const [userAmount, setUserAmount] = useState("");
+  const [isUsdInput, setIsUsdInput] = useState(false); // Added state variable
+  const [usdAmount, setUsdAmount] = useState(""); // Added state variable
+  const [currencyName, setCurrencyName] = useState("ETH");
+
 
   useEffect(() => {
     getCurrentWalletConnected();
     addWalletListener();
   }, [walletAddress, walletChainId]);
 
-  const connectWallet = async () => {
-    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      try {
-        /* MetaMask is installed */
-        /* fetch account */
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setWalletAddress(accounts[0]);
-        console.log(accounts[0]);
-        /* fetch chainId */
-        const chainId = await window.ethereum.request({
-          method: 'eth_chainId'
-        });
-        setWalletChainId(chainId);
-        console.log(chainId);
-      } catch (err) {
-        console.error(err.message);
+  useEffect(() => {
+    const fetchCurrencyName = async () => {
+      if (walletChainId) {
+        const currencyName = chainIdToCurrencyCode(walletChainId);
+        setCurrencyName(currencyName);
       }
+    };
+    fetchCurrencyName();
+  }, [walletChainId]);
+
+  const RequestConnection = async () => {
+    var msg = "";
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      msg = "Please install and connect to Metamask to continue.";
     } else {
+      msg = "Please connect to Metamask to continue.";
+    }
+    console.log(msg);
+    window.alert(msg);
+  }
+
+  const connectWallet = async () => {
+    if (!(typeof window != "undefined" && typeof window.ethereum != "undefined")) {
       /* MetaMask is not installed */
-      console.log("Please install MetaMask");
+      RequestConnection();
+      return;
+    }
+    try {
+      /* MetaMask is installed */
+      /* fetch account */
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
+      console.log(accounts[0]);
+      /* fetch chainId */
+      const chainId = await window.ethereum.request({
+        method: 'eth_chainId'
+      });
+      setWalletChainId(chainId);
+      console.log(chainId);
+
+    } catch (err) {
+      console.error(err.message);
     }
   };
 
+
+
   const getCurrentWalletConnected = async () => {
-    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_accounts",
-        });
-        if (accounts.length > 0) {
-          setWalletAddress(accounts[0]);
-          console.log(accounts[0]);
-        } else {
-          console.log("Connect to MetaMask using the Connect button");
-          return;
-        }
-        /* fetch chainId */
-        const chainId = await window.ethereum.request({
-          method: 'eth_chainId'
-        });
-        if (chainId.length > 0) { /* if we are here, should be always true!? */
-          setWalletChainId(chainId);
-          console.log(chainId);
-        } else {
-          console.log("Connect to MetaMask using the Connect button");
-        }
-      } catch (err) {
-        console.error(err.message);
-      }
-    } else {
+    if (!(typeof window != "undefined" && typeof window.ethereum != "undefined")) {
       /* MetaMask is not installed */
-      console.log("Please install MetaMask");
+      RequestConnection();
+      return;
+    }
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (accounts.length > 0) {
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } else {
+        RequestConnection();
+        return;
+      }
+      /* fetch chainId */
+      const chainId = await window.ethereum.request({
+        method: 'eth_chainId'
+      });
+      if (chainId.length > 0) { /* if we are here, should be always true!? */
+        setWalletChainId(chainId);
+        console.log(chainId);
+      } else {
+        RequestConnection();
+      }
+    } catch (err) {
+      console.error(err.message);
     }
   };
 
   const addWalletListener = async () => {
-    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        setWalletAddress(accounts[0]);
-        console.log(accounts[0]);
-      });
-      window.ethereum.on("chainChanged", (chainId) => {
-        setWalletAddress(chainId);
-        console.log(chainId);
-      });
-    } else {
+    if (!(typeof window != "undefined" && typeof window.ethereum != "undefined")) {
       /* MetaMask is not installed */
       setWalletAddress("");
-      console.log("Please install MetaMask");
+      RequestConnection();
+      return;
     }
+    window.ethereum.on("accountsChanged", (accounts) => {
+      setWalletAddress(accounts[0]);
+      console.log(accounts[0]);
+    });
+    window.ethereum.on("chainChanged", (chainId) => {
+      setWalletAddress(chainId);
+      console.log(chainId);
+    });
   };
 
   const onInputChange = async event => {
+    if (!(walletChainId && walletAddress && walletChainId.length > 0 && walletAddress.length > 0)) {
+      RequestConnection()
+      return;
+    }
     if (event.target.validity.valid) {
       setUserAmount(event.target.value);
+      setIsUsdInput(false);
+
+      if (event.target.value === "") {
+        setUsdAmount("");
+      } else {
+        const usdValue = await convertToUsd(event.target.value, walletChainId);
+        setUsdAmount(usdValue);
+      }
     }
   };
+
+  const onUsdInputChange = async event => {
+    if (!(walletChainId && walletAddress && walletChainId.length > 0 && walletAddress.length > 0)) {
+      RequestConnection()
+      return;
+    }
+    if (event.target.validity.valid) {
+      setUsdAmount(event.target.value);
+      setIsUsdInput(true);
+
+      if (event.target.value === "") {
+        setUserAmount("");
+      } else {
+        const currencyValue = await convertToCurrency(event.target.value, walletChainId);
+        setUserAmount(currencyValue);
+      }
+    }
+  };
+
 
   const handleDonate = async event => {
 
       if (!(walletChainId && walletAddress && walletChainId.length > 0 && walletAddress.length > 0)) {
-        const msg = 'Please try to connect first!';
-        console.log(msg);
-        return alert(msg);
+        RequestConnection()
+        return;
       }
       console.log(userAmount);
       var EtherToWei = 0;
@@ -165,17 +224,33 @@ function App() {
             <div className="box address-box">
               <div className="columns">
                 <div className="column is-four-fifths">
-                  <input
-                    className="input is-medium"
-                    type="text"
-                    value={userAmount}
-                    pattern="[0-9]+([\.][0-9]*)?" 
-                    onInput={onInputChange}
-                    placeholder="Enter the amount (example 0.05)"
-                  />
+                  <div className="columns">
+                    <div className="column is-half">
+                      <label className="label">Enter amount in ${currencyName}:</label>
+                      <input
+                        className="input is-medium"
+                        type="text"
+                        value={userAmount}
+                        pattern="[0-9]+([\.][0-9]*)?"
+                        onInput={onInputChange}
+                        placeholder="Enter the amount (example 0.05)"
+                      />
+                    </div>
+                    <div className="column is-half">
+                      <label className="label">Amount in USD:</label>
+                      <input
+                        className="input is-medium"
+                        type="text"
+                        value={usdAmount}
+                        pattern="[0-9]+([\.][0-9]*)?"
+                        onInput={onUsdInputChange}
+                        placeholder="Enter the amount in USD"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="column">
-                  <button className="button is-link is-medium" onClick={handleDonate} >
+                <div className="column" style={{ paddingTop: "2rem" }}>
+                  <button className="button is-link is-large" onClick={handleDonate} >
                     DONATE
                   </button>
                 </div>
